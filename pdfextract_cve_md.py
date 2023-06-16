6fba91915d3e90794205ec90a7cb9d1bc39cf82b
Dia Seung <diana.seung@liferay.com>
2023-06-16 11:38:04 -0700

    input validation for isfile and pdf extension

--- pdfextract_cve_md.py
+++ pdfextract_cve_md.py
@@ -7,22 +7,26 @@ import re
 import os
 from os import path
 
-# Define Output File
+# Define raw PDF text Output File
 raw_output = './output/raw_pdf_extract.txt'
 
 # Define Input Directory /  File
-file_input = input('Enter path to input file: ')
+file_input = input('[Enter path to PDF file]: ')
 
-while path.isfile(file_input) == False:
-    "Invalid file: "+str(file_input)
-    file_input = input('Enter valid path to input file: ')
+input_name, input_extension = path.splitext(file_input)
+
+while (path.isfile(file_input) == False) or (input_extension.lower() != '.pdf'):
+    print("ERROR: Invalid file -", file_input, "(extension:", input_extension, ")\n")
+    file_input = input('[Enter valid path to PDF file]: ')
+    input_name, input_extension = path.splitext(file_input)
+    print("INFO: New input file -", file_input, "(extension:", input_extension, ")")
 else:
     # Open the file and create a pdf object.
     pdf = pdfplumber.open(file_input)
 
     # Get and Print the number of pages.
     numPages = len(pdf.pages)
-    print("Number of Pages:", numPages)
+    print("INFO: Extracting", numPages, "pages of raw text from", file_input, "(extension:", input_extension, ")")
 
     # Iterate over each page and extract the text of each page.
     for number, pageText in enumerate(pdf.pages):
@@ -30,10 +34,11 @@ else:
         print(pageText.extract_text(), file=open(raw_output,'a'))
     print("SUCCESS: Text extracted from", file_input, "to", raw_output)
 
-# This extracts only the CVEs from a raw_output
+# Define CVE list Output File
 cve_output = './output/cve_extract.txt'
+
+# This extracts only the CVEs from a raw_output
 with open (raw_output, 'rt') as pdf_extract:
-    print("INFO: Finding all CVE in", raw_output)
     for line in pdf_extract:
         regex = r'CVE-\d{4,5}-\d{4,7}'
         regex_find = re.compile(regex)
@@ -45,7 +50,6 @@ with open (raw_output, 'rt') as pdf_extract:
 
 # This reformats a txt list of CVEs into a Markdown-compatible list with NVD links
 with open (cve_output, 'rt') as cve_list:
-    print("INFO: Converting CVE list to numbered list of hyperlinked Markdown")
     for cve in cve_list:
         cleaned_cve = cve.strip().replace(' ', '')
         md_start = '# ['
