b18ae702e0d5f4e5a73900c97c69eaef91326c47
Dia Seung <diana.seung@liferay.com>
2023-06-16 11:38:52 -0700

    update step 1 to match compiled

--- 01_get_pdf_raw.py
+++ 01_get_pdf_raw.py
@@ -8,19 +8,22 @@ from os import path
 raw_output = './output/raw_pdf_extract.txt'
 
 # Define Input Directory /  File
-# file_input = './input/input.pdf'
-file_input = input('Enter path to input file: ')
+file_input = input('[Enter path to PDF file]: ')
 
-while path.isfile(file_input) == False:
-    "Invalid file: "+str(file_input)
-    file_input = input
+input_name, input_extension = path.splitext(file_input)
+
+while (path.isfile(file_input) == False) or (input_extension.lower() != '.pdf'):
+    print("ERROR: Invalid file -", file_input, "(extension:", input_extension, ")\n")
+    file_input = input('[Enter valid path to PDF file]: ')
+    input_name, input_extension = path.splitext(file_input)
+    print("INFO: New input file -", file_input, "(extension:", input_extension, ")")
 else:
     # Open the file and create a pdf object.
     pdf = pdfplumber.open(file_input)
 
     # Get and Print the number of pages.
     numPages = len(pdf.pages)
-    print("Number of Pages:", numPages)
+    print("INFO: Extracting", numPages, "pages of raw text from", file_input, "(extension:", input_extension, ")")
 
     # Iterate over each page and extract the text of each page.
     for number, pageText in enumerate(pdf.pages):
