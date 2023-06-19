1c81b99fc8a0128f1357af2d26f37e494e3cec03
Dia Seung <diana.seung@liferay.com>
2023-06-19 16:48:42 -0700

    added file check, no override if output exist

--- package/get_raw.py
+++ package/get_raw.py
@@ -20,17 +20,20 @@ def get_raw():
         input_name, input_extension = path.splitext(file_input)
         print("INFO: New input file -", file_input, "(extension:", input_extension, ")")
     else:
-        # Open the file and create a pdf object.
-        pdf = pdfplumber.open(file_input)
+        if (path.isfile(raw_output) == False):
+            # Open the file and create a pdf object.
+            pdf = pdfplumber.open(file_input)
 
-        # Get and Print the number of pages.
-        numPages = len(pdf.pages)
-        print("INFO: Extracting", numPages, "pages of raw text from", file_input, "(extension:", input_extension, ")")
+            # Get and Print the number of pages.
+            numPages = len(pdf.pages)
+            print("INFO: Extracting", numPages, "pages of raw text from", file_input, "(extension:", input_extension, ")")
 
-        # Iterate over each page and extract the text of each page.
-        for number, pageText in enumerate(pdf.pages):
-            print("Page Number:", number, file=open(raw_output, 'a'))
-            print(pageText.extract_text(), file=open(raw_output,'a'))
-        print("SUCCESS: Text extracted from", file_input, "to", raw_output)
+            # Iterate over each page and extract the text of each page.
+            for number, pageText in enumerate(pdf.pages):
+                print("Page Number:", number, file=open(raw_output, 'a'))
+                print(pageText.extract_text(), file=open(raw_output,'a'))
+            print("SUCCESS: Text extracted from", file_input, "to", raw_output)
+        else:
+            print('File already exists')
 
 get_raw()
