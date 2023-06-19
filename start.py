06a7573211dd296cdc17bac0832182a16ac4ad08
Dia Seung <diana.seung@liferay.com>
2023-06-19 16:48:06 -0700

    added package reload

--- start.py
+++ start.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # Compiled one-step script to extract CVEs from a PDF and prepare them with markdown
+from importlib import reload
 
 import package.get_raw
 import package.extract_cve
 import package.markdown_cve
+
+
+package = reload(package)
 
 package.get_raw
 package.extract_cve
