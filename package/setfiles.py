5f32468a5ac344519c1676dbbfd25c84c4976f7b
dianaseung <diana.seung@liferay.com>
2023-06-20 15:23:58 -0700

    add timestamp to output

--- package/setfiles.py
+++ package/setfiles.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+import datetime
+timestamp = datetime.datetime.now().strftime('%y%m%d_%H%M%S')
 
 # Define raw PDF text Output File
-raw_output = 'output/01_raw_pdf_extract.txt'
+raw_output = 'output/' + timestamp + '/01_raw_pdf_extract.txt'
 
 # Define CVE list Output File
-cve_output = 'output/02_cve_extract.txt'
+cve_output = 'output/' + timestamp + '/02_cve_extract.txt'
 
 # Define Markdown Output File
-md_output = 'output/03_md_cve_list.txt'
+md_output = 'output/' + timestamp + '/03_md_cve_list.txt'
