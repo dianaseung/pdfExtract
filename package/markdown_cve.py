aad31233a9a582ebdc1b21d7ce67f7d2536015b6
Dia Seung <diana.seung@liferay.com>
2023-06-19 18:12:08 -0700

    option to choose between jira or excel formatting

--- package/markdown_cve.py
+++ package/markdown_cve.py
@@ -8,10 +8,14 @@ from package.setfiles import cve_output as cve_output
 from package.setfiles import md_output as md_output
 
 def md_cve():
+    format  = input("Format as (jira or excel): ")
+    while format.lower() not in {"jira", "excel"}:
+        format = input("Format as (jira or excel): ")
+
     # Open the file and create a pdf object.
     with open (cve_output, 'rt') as cve_list:
         x = 1
-        y = 1
+        y = 0
         for cve in cve_list:
             # Regex for checking if line is a CVE
             regex = r'CVE-\d{4,5}-\d{4,7}'
@@ -20,17 +24,28 @@ def md_cve():
             # If line matches CVE regex, convert it into an ordered list with CVE URL link.
             if regex_find.match(cve):
                 # Include ordered list in start
-                md_start = str(x) + '. ['
-                md_url  = '](https://nvd.nist.gov/vuln/detail/'
-                md_end = ')'
+                # md_start = str(x) + '. ['
+                if format.lower() == 'excel':
+                    md_start = '=HYPERLINK("https://nvd.nist.gov/vuln/detail/'
+                    md_url  = '", "'
+                    md_end = '")'
+                elif format.lower() == 'jira':
+                    md_start = str(x) + '. ['
+                    md_url  = '](https://nvd.nist.gov/vuln/detail/'
+                    md_end = ')'
+                else:
+                    md_start = str(x) + '. ['
+                    md_url  = '|https://nvd.nist.gov/vuln/detail/'
+                    md_end = ']'
                 print(md_start.strip(), cleaned_cve.strip(), md_url.strip(), cleaned_cve.strip(), md_end.strip(), sep="", file=open(md_output,'a'))
                 x = x+1
             else:
                 # If not CVE, format it as follows. Currently set to h2 heading
-                md_start = '## ' + str(y) + '.'
-                print(md_start, cleaned_cve.strip(), file=open(md_output, 'a'))
+                md_start = ' ' + str(y) + '.'
+                md_end = '|'
+                print(cleaned_cve.strip(), file=open(md_output, 'a'))
                 y = y+1
     if (os.path.isfile(md_output) == True):
-        print("SUCCESS: Markdown format applied to CVE list from", cve_output, "to", md_output)
+        print("SUCCESS: ", format, "formatting applied to CVE list from", cve_output, "to", md_output)
 
 md_cve()
